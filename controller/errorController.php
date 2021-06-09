8f00181cdf8eff5065c0ba7d69e4bd99d77e2402
pras <pptech.ds@gmail.com>
2021-06-09 09:49:21 +0200

    bug fix in controller error

--- controller/errorController.php
+++ controller/errorController.php
@@ -1,6 +1,11 @@
 <?php
 
 function error($e)
-{
-    require dirname(__DIR__) . 'view/error/error' . $e->getMessage() . '.php';
+{   
+    ob_start();
+    require dirname(__DIR__) . '/view/error/error' . $e->getMessage() . '.php';
+    $content = ob_get_clean();
+
+    require dirname(__DIR__) . '/view/base.php';
+
 }
--- view/error/error404.php
+++ view/error/error404.php
@@ -1,4 +1,4 @@
-<div class="alert alert-dismissible alert-warning">
+<div class="alert alert-dismissible alert-warning mt-5">
     <h4 class="alert-heading">Erreur</h4>
     <p class="mb-0">Erreur 404 : La page demandée est introuvable !</p>
 </div>
